bee5d4a4c4d24548c4bde37bf64ea5435eb1eddb
Joel Joby <joeljoby28430@gmail.com>
2023-09-23 12:20:55 +0530

    just echo

---
+++ application/controllers/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+    class Test extends CI_Controller
+    {
+        public function welcome()
+        {
+            echo "welcome to Codeigniter";
+        }
+
+        public function greetings()
+        {
+            echo "u are using CodeIgniter 3";
+        }
+    }
+
+?>
