91a4afc107476b8433f054b92a4cc0e70b54963b
Joel Joby <joeljoby28430@gmail.com>
2023-11-01 11:16:49 +0530

    form_continuation

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -8,9 +8,11 @@
 
             $this -> load -> library("form_validation");
 
+
             $this -> form_validation -> set_rules("name","Name","required|min_length[3]|alpha");
-            $this -> form_validation -> set_rules("email","Email","required|valid_email");
+            $this -> form_validation -> set_rules("email","Email","required|valid_email", array('required' => 'Email required','valid_email' => 'Email Required'));
             $this -> form_validation -> set_rules("mobile","Mobile","required|numeric|exact_length[10]");
+
 
             if($this -> form_validation -> run() === TRUE )
             {
--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -8,23 +8,33 @@
 <body>
 
     <h1>Contact Form</h1>
-    <?php echo validation_errors(); ?>
+    <?php //echo validation_errors(); ?>
 
     <?php echo form_open(); ?>
     <Table>
         <tr>
             <td>Name</td>
-            <td><input type = "text" name ="name" id ="name" value = "<?php echo set_value('name'); ?>"></td>
+            <td>
+                <input type = "text" name ="name" id ="name" value = "<?php echo set_value('name'); ?>">
+                <span><?php echo form_error('name'); ?></span>
+            </td>
         </tr>
 
         <tr>
             <td>Email Address</td>
-            <td><input type = "text" name = "email" id = "email" value = "<?php echo set_value('email'); ?>"></td>
+            <td>
+                <input type = "text" name = "email" id = "email" value = "<?php echo set_value('email'); ?>">
+                <span><?php echo form_error('email'); ?></span>
+            </td>
         </tr>
 
         <tr>
             <td>Mobile</td>
-            <td><input type = "text" name = "mobile" id = "mobile" value = "<?php echo set_value('mobile'); ?>"></td>
+            <td>
+                
+                <input type = "text" name = "mobile" id = "mobile" value = "<?php echo set_value('mobile'); ?>">
+                <span><?php echo form_error('mobile'); ?></span>
+            </td>
         </tr>
 
         <!-- <tr>
